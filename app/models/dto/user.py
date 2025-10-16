a5384d338c68244b98c87bcb127ca1ebcd20d74b
Daniel <d.akipskiy@gmail.com>
2025-10-16 11:58:00 +0800

    feat: add is_super_admin to admin's table

--- app/models/dto/user.py
+++ app/models/dto/user.py
@@ -13,11 +13,21 @@ class AdminUserDto(ActiveRecordModel):
     password_hash: str = Field(..., alias="password")
     is_blocked: bool = False
 
+    ## Akeeper 16.10.2025
+    is_super_admin: bool = False
+    ## ~Akeeper
+
     def check_password(self, password: str) -> bool:
         """
         Проверка, соответствует ли предоставленный пароль сохраненному хешу.
         """
         return bcrypt.checkpw(password.encode("utf-8"), self.password_hash.encode("utf-8"))
+
+
+class AdminUserCreateDto(ActiveRecordModel):
+    name: str
+    username: str
+    is_superuser: bool = False
 
 
 class UserDto(ActiveRecordModel):
---
+++ migrations/versions/2025_10_16_1156-89022f1cb3df_add_is_super_admin_to_admin_table.py
@@ -0,0 +1,60 @@
+"""add is_super_admin to admin table
+
+Revision ID: 89022f1cb3df
+Revises: 0c1406fbafcb
+Create Date: 2025-10-16 11:56:49.541138
+
+"""
+from typing import Optional, Sequence
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "89022f1cb3df"
+down_revision: Optional[str] = "0c1406fbafcb"
+branch_labels: Optional[Sequence[str]] = None
+depends_on: Optional[Sequence[str]] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "admin", sa.Column("is_super_admin", sa.Boolean(), server_default="false", nullable=False),
+        schema="users",
+    )
+    op.alter_column(
+        "admin", "user_id",
+        existing_type=sa.BIGINT(),
+        type_=sa.Integer(),
+        existing_nullable=False,
+        autoincrement=True,
+        schema="users",
+    )
+    op.alter_column(
+        "user", "user_id",
+        existing_type=sa.BIGINT(),
+        type_=sa.Integer(),
+        existing_nullable=False,
+        autoincrement=True,
+        schema="users",
+    )
+
+
+def downgrade() -> None:
+    op.alter_column(
+        "user", "user_id",
+        existing_type=sa.Integer(),
+        type_=sa.BIGINT(),
+        existing_nullable=False,
+        autoincrement=True,
+        schema="users",
+    )
+    op.alter_column(
+        "admin", "user_id",
+        existing_type=sa.Integer(),
+        type_=sa.BIGINT(),
+        existing_nullable=False,
+        autoincrement=True,
+        schema="users",
+    )
+    op.drop_column("admin", "is_super_admin", schema="users")
